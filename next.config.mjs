5f4a3e5cf82594d55f76427f261e291584cd5447
wendy8-29 <jojin0921@gmail.com>
2025-09-24 15:23:22 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 // next.config.mjs
-const repo = 'wendy_closet_'; // 예: 'wardrobe'
+const repo = 'wendy_closet_'; // ← 리포 이름과 정확히 동일!
 const isProd = process.env.NODE_ENV === 'production';
 
 export default {
-  output: 'export',              // GitHub Pages용 정적 내보내기
-  images: { unoptimized: true }, // next/image 최적화 끄기 (정적 호환)
-  basePath: isProd ? `/${wendy_closet_}` : '',
-  assetPrefix: isProd ? `/${wendy_closet_}/` : '',
-  // 필요시: trailingSlash: true,
+  output: 'export',              // 핵심: 정적 내보내기 ON
+  images: { unoptimized: true }, // next/image 경고 방지
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
 };
