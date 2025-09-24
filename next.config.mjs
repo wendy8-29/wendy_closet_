b86013846810017bde593bc8470df6a154a2198c
wendy8-29 <jojin0921@gmail.com>
2025-09-24 14:47:07 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,10 @@
+const repo = 'wendy_closet_'; // ← repo 이름과 동일하게
+const isProd = process.env.NODE_ENV === 'production';
 
+export default {
+  output: 'export',                // 정적 내보내기
+  images: { unoptimized: true },   // next/image 비활성 (정적 호환)
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : ''
+  // 필요하면 trailingSlash: true,
+};
